e6901354b7be69de915b3c1296183bf1ba85902a
André Santiago <andre.mpc.santiago@gmail.com>
2023-02-04 14:46:46 +0100

    feat: added initial roots implementation

---
+++ Assets/Game/Gameplay/Roots/Root.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+
+namespace Game.Gameplay.Roots
+{
+    public class Root : MonoBehaviour
+    {
+        [SerializeField]
+        private AnimationCurve attachmentForceOverTime = AnimationCurve.Linear(0, 1, 60, 10);
+
+        [SerializeField]
+        private AnimationCurve attachmentDistanceOverTime = AnimationCurve.Linear(0, 10, 60, 2);
+
+        private Root _attachedRoot;
+
+        private Collider _collider;
+        public Rigidbody Rigidbody { get; private set; }
+
+        public bool Attached { get; private set; }
+        public float AttachmentStartMoment { get; private set; }
+
+        public float AttachmentForce => attachmentForceOverTime.Evaluate(Time.fixedTime - AttachmentStartMoment);
+        public float AttachmentDistance => attachmentDistanceOverTime.Evaluate(Time.fixedTime - AttachmentStartMoment);
+
+        private void Awake ()
+        {
+            Rigidbody = transform.parent.GetComponentInParent<Rigidbody>();
+            _collider = GetComponent<Collider>();
+        }
+
+        private void FixedUpdate ()
+        {
+            if (!Attached) {
+                _collider.enabled = true;
+                return;
+            }
+
+            Vector3 displacement = transform.position - _attachedRoot.transform.position;
+            float attachmentDistance = AttachmentDistance;
+            if (displacement.sqrMagnitude > attachmentDistance * attachmentDistance) {
+                float attachmentForce = AttachmentForce;
+                Vector3 forceDirection = displacement.normalized;
+
+                _attachedRoot.Rigidbody.AddForceAtPosition(forceDirection * attachmentForce, _attachedRoot.transform.position, ForceMode.Force);
+            }
+        }
+
+        private void OnCollisionEnter (Collision collision)
+        {
+            if (Attached)
+                return;
+
+            bool success = collision.gameObject.TryGetComponent(out Root otherRoot);
+            if (!success || otherRoot.Attached)
+                return;
+            // get distance between roots
+            Debug.Log((transform.position - otherRoot.transform.position).magnitude);
+            
+            Attached = true;
+            _attachedRoot = otherRoot;
+            AttachmentStartMoment = Time.fixedTime;
+            _collider.enabled = false;
+            otherRoot.Attach(this);
+        }
+
+        public void Attach (Root otherRoot) => Attach(otherRoot, Time.fixedTime);
+
+        public void Attach (Root otherRoot, float attachmentStartMoment)
+        {
+            Attached = true;
+            _attachedRoot = otherRoot;
+            AttachmentStartMoment = attachmentStartMoment;
+            _collider.enabled = false;
+        }
+
+        public Root Detach ()
+        {
+            Root otherRoot = _attachedRoot;
+            _attachedRoot = null;
+            _collider.enabled = true;
+            Attached = false;
+
+            otherRoot.Detach();
+
+            return otherRoot;
+        }
+    }
+}
---
+++ Assets/Game/Gameplay/Roots/RootLocation.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Game.Gameplay.Roots
+{
+    public class RootLocation : MonoBehaviour
+    {
+        [SerializeField, Min(0)]
+        private float growthInterval = 10f;
+
+        [SerializeField]
+        private List<Root> rootStages;
+
+        private Root _currentRoot;
+
+        private void Start ()
+        {
+            Grow(rootStages[0]);
+        }
+
+        private void Grow (Root nextRootStage)
+        {
+            float attachMoment = 0;
+            Root otherRoot = null;
+            if (_currentRoot != null) {
+                if (_currentRoot.Attached) {
+                    attachMoment = _currentRoot.AttachmentStartMoment;
+                    otherRoot = _currentRoot.Detach();
+                }
+
+                Destroy(_currentRoot.gameObject);
+            }
+
+            _currentRoot = Instantiate(nextRootStage, transform, false);
+
+            if (otherRoot != null)
+                _currentRoot.Attach(otherRoot, attachMoment);
+        }
+    }
+}
