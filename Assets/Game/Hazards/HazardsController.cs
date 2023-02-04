fbdd36d64b92f6e18ab9eb0ea8c3fa91430c6225
André Santiago <andre.mpc.santiago@gmail.com>
2023-02-04 03:35:58 +0100

    feat: added initial implementation of general hazards controller

---
+++ Assets/Game/Hazards/HazardsController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Game.Hazards
+{
+    public class HazardsController : MonoBehaviour
+    {
+        [SerializeField]
+        private float spawnInterval = 5f;
+
+        private IHazardController[] _hazardControllers;
+
+        private void Awake ()
+        {
+            _hazardControllers = GetComponentsInChildren<IHazardController>();
+        }
+
+        private void Start ()
+        {
+            StartCoroutine(SpawnHazard());
+        }
+
+        private IEnumerator SpawnHazard ()
+        {
+            while (true) {
+                int i = Random.Range(0, _hazardControllers.Length);
+                _hazardControllers[i].SpawnHazard(transform);
+
+                yield return new WaitForSeconds(spawnInterval);
+            }
+        }
+    }
+}
